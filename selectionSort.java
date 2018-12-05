408d779d50d3cd0c018090c2ae1c77fba641973b
angeliquecd <acharles-davis00@stuy.edu>
2018-12-04 21:21:50 -0500

    added comments

--- selectionSort.java
+++ selectionSort.java
@@ -1,20 +1,20 @@
 public class selectionSort{
 public static void selectionSort(int [] ary) {
-  int shortest=ary[0];
+  int shortest=ary[0];//initializes variables with random values
   int old = 4;
   int shortestindex=-5;
-  for (int index=0;index<ary.length;index++){
-        shortest = ary[index];
-        shortestindex=index;
+  for (int index=0;index<ary.length;index++){//fills in index by index
+        shortest = ary[index];//rests these from last loop through
+        shortestindex=index;//excludes past values
         old=ary[index];
   for (int i=0;i+index<ary.length;i++){
-    if (ary[index+i]<shortest) {
-    shortest=ary[i+index];
-    shortestindex=index+i;}
+    if (ary[index+i]<shortest) {//finds shortest part of ary
+    shortest=ary[i+index];//saves what it is
+    shortestindex=index+i;}//bookmarks its location
   }
-  ary[index]=shortest;
+  ary[index]=shortest;//performs a swap:
   ary[shortestindex]=old;
-  System.out.println(printArray(ary));
+  //System.out.println(printArray(ary))
   }
 }
   public static String printArray(int[] ary) {
